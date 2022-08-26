ff074e78fe3a95d425101f216e7d4f2ff23cdc81
Anish Kanna <sautronik@gmail.com>
2022-08-26 16:34:10 +0530

    fixed bugs

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
 import * as THREE from 'three'
-import { Canvas } from '@react-three/fiber'
+import { Canvas, useFrame } from '@react-three/fiber'
 import { Cloud, OrbitControls, Sky, Environment, useGLTF } from '@react-three/drei'
 import { Physics, RigidBody, Debug, RapierRigidBody } from '@react-three/rapier'
-import { Suspense, useRef } from 'react'
+import { Suspense, useEffect, useRef, useState } from 'react'
 
 import { useControls } from 'leva'
-import { BufferGeometry, Mesh, Material } from 'three'
+import { BufferGeometry, Mesh, Material, Vector3 } from 'three'
 import { RigidBodyApi } from '@react-three/rapier/dist/declarations/src/types'
 
 const App = () => {
 
   const { debug } = useControls({ debug: false })
+
+
+
   return (
-    <Canvas shadows camera={{ position: [-50, -25, 150], fov: 15 }}>
+    <Canvas shadows camera={{ position: [-50, 40, 100], fov: 25 }}>
       <Suspense fallback={null}>
         <hemisphereLight intensity={0.45} />
         <spotLight angle={0.4} penumbra={1} position={[20, 30, 2.5]} castShadow shadow-bias={-0.00001} />
@@ -75,7 +78,7 @@ const Track = (props: any) => {
 const Sphere = (props: any) => {
   return (
     <RigidBody
-      colliders='ball' restitution={0.7}
+      colliders='ball' restitution={.65}
     >
       <mesh castShadow receiveShadow {...props}>
         <sphereGeometry args={[0.5, 32, 32]} />
@@ -102,13 +105,27 @@ const Box = ({ length = 4, ...props }) => {
 
 const Pacman = ({ position }: any) => {
   const pacmanRef = useRef() as any
+  useFrame((state) => {
+    const t = state.clock.getElapsedTime()
+    pacmanRef.current.setNextKinematicTranslation({ x: -5 + Math.sin(t * -10), y: -8 + Math.sin(t * 10) / 2, z: 0 })
+
+    pacmanRef.current.setNextKinematicRotation({ x: Math.cos(t) * 0.2, y: Math.sin(t) * 0.1 - 0.1, z: Math.cos(t) * 0.05 })
+  })
   return (
     <group position={position}>
-
       <RigidBody ref={pacmanRef} type='kinematicPosition' colliders='trimesh' >
         <mesh castShadow receiveShadow rotation={[-Math.PI / 2, Math.PI, 0]}>
           <sphereGeometry args={[10, 32, 32, 0, Math.PI * 1.3]} />
-          <meshStandardMaterial color={'#ffc060'} side={THREE.DoubleSide} />
+          <meshStandardMaterial color={'#E64848'} side={THREE.DoubleSide} />
+        </mesh>
+        <mesh position={[-5, 0, 8.5]} castShadow >
+          <sphereGeometry args={[1, 16, 16]} />
+          <meshStandardMaterial color='white' roughness={0.75} />
+        </mesh>
+
+        <mesh position={[-5, 0, -8.5]} castShadow >
+          <sphereGeometry args={[1, 16, 16]} />
+          <meshStandardMaterial color='white' roughness={0.75} />
         </mesh>
       </RigidBody>
 
